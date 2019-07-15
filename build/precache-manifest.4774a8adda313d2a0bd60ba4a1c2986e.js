a530c4ea1c1fd7c0824c3e13a2a3fb58fe5b425e
Hmd karimi <hamidreza.karimi.r@gmail.com>
2019-07-16 01:11:49 +0430

    add build folder

---
+++ build/precache-manifest.4774a8adda313d2a0bd60ba4a1c2986e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d41745f7a9e532d706a2712b16d8026d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aeb6970dce651792a6c9",
+    "url": "/static/css/main.c787bfbb.chunk.css"
+  },
+  {
+    "revision": "8f79e1637f206bdebdee",
+    "url": "/static/js/2.dae933d0.chunk.js"
+  },
+  {
+    "revision": "aeb6970dce651792a6c9",
+    "url": "/static/js/main.c3350a6a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "22a401c13cb625b633a1daa4adf01a24",
+    "url": "/static/media/1.22a401c1.jpg"
+  },
+  {
+    "revision": "5cfbd7d438f2c08f94035428edf68ab3",
+    "url": "/static/media/android.5cfbd7d4.svg"
+  },
+  {
+    "revision": "3854c66fb81e14b6905ad15297c0775c",
+    "url": "/static/media/ios.3854c66f.svg"
+  },
+  {
+    "revision": "85875dbddaf0535a7a3848f53123c83d",
+    "url": "/static/media/logo.85875dbd.svg"
+  },
+  {
+    "revision": "f348c552bb53aa77d8b06c91a466dd60",
+    "url": "/static/media/logo2.f348c552.svg"
+  },
+  {
+    "revision": "399fdc3f49c93c1c29d7e3bdf4b82077",
+    "url": "/static/media/p1.399fdc3f.svg"
+  },
+  {
+    "revision": "5886363eb410836eab8e349db68d7276",
+    "url": "/static/media/p2.5886363e.svg"
+  },
+  {
+    "revision": "a6b8e27fed2ab2b936550b38c470dc6a",
+    "url": "/static/media/p3.a6b8e27f.svg"
+  },
+  {
+    "revision": "a45a2d5dc2057e0a76369d69d4a93968",
+    "url": "/static/media/s1.a45a2d5d.svg"
+  },
+  {
+    "revision": "165b4ab2eb9558c41d003f83a528a0b2",
+    "url": "/static/media/s2.165b4ab2.svg"
+  }
+]);
